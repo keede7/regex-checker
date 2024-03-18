98431d2b08dd817b5217c10d8335efffa9cc87d1
keede <wesaq11@gmail.com>
2024-03-18 16:18:31 +0900

    Sign up UserId Pattern Test

--- src/test/java/io/keede7/pattern/SignupPatternTestModelTests.java
+++ src/test/java/io/keede7/pattern/SignupPatternTestModelTests.java
@@ -3,7 +3,9 @@ package io.keede7.pattern;
 import io.keede7.util.config.TestValidator;
 import org.junit.jupiter.api.Test;
 
-import static io.keede7.pattern.model.SignupPatternTestModel.EMAIL_PATTERN;
+import java.util.regex.Pattern;
+
+import static io.keede7.pattern.model.SignupPatternTestModel.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -15,27 +17,27 @@ class SignupPatternTestModelTests extends TestValidator {
     @Test
     void emailPatternSuccessTest() {
 
-        final String successModel1 = "weasq11@gmail.com";
-        final String successModel2 = "weasq@gmail.com";
-        final String successModel3 = "1293821@gmail.com";
-        final String successModel4 = "1@gmail.com";
-        final String successModel5 = "d@gmail.com";
+        final String model1 = "weasq11@gmail.com";
+        final String model2 = "weasq@gmail.com";
+        final String model3 = "1293821@gmail.com";
+        final String model4 = "1@gmail.com";
+        final String model5 = "d@gmail.com";
 
         assertAll(
                 () -> assertTrue(
-                        isCheck(EMAIL_PATTERN, successModel1)
+                        isCheck(EMAIL_PATTERN, model1)
                 ),
                 () -> assertTrue(
-                        isCheck(EMAIL_PATTERN, successModel2)
+                        isCheck(EMAIL_PATTERN, model2)
                 ),
                 () -> assertTrue(
-                        isCheck(EMAIL_PATTERN, successModel3)
+                        isCheck(EMAIL_PATTERN, model3)
                 ),
                 () -> assertTrue(
-                        isCheck(EMAIL_PATTERN, successModel4)
+                        isCheck(EMAIL_PATTERN, model4)
                 ),
                 () -> assertTrue(
-                        isCheck(EMAIL_PATTERN, successModel5)
+                        isCheck(EMAIL_PATTERN, model5)
                 )
         );
     }
@@ -43,33 +45,93 @@ class SignupPatternTestModelTests extends TestValidator {
     @Test
     void emailPatternFailTest() {
 
-        final String failModel1 = "테스트@gmail.com";
-        final String failModel2 = "테스트23@gmail.com";
-        final String failModel3 = "@sdf@gmail.com";
-        final String failModel4 = "sdf@gmailcom";
-        final String failModel5 = "sdf@.com";
-        final String failModel6 = "sdf@gamil.";
+        final String model1 = "테스트@gmail.com";
+        final String model2 = "테스트23@gmail.com";
+        final String model3 = "@sdf@gmail.com";
+        final String model4 = "sdf@gmailcom";
+        final String model5 = "sdf@.com";
+        final String model6 = "sdf@gamil.";
 
         assertAll(
                 () -> assertFalse(
-                        isCheck(EMAIL_PATTERN, failModel1)
+                        isCheck(EMAIL_PATTERN, model1)
                 ),
                 () -> assertFalse(
-                        isCheck(EMAIL_PATTERN, failModel2)
+                        isCheck(EMAIL_PATTERN, model2)
                 ),
                 () -> assertFalse(
-                        isCheck(EMAIL_PATTERN, failModel3)
+                        isCheck(EMAIL_PATTERN, model3)
                 ),
                 () -> assertFalse(
-                        isCheck(EMAIL_PATTERN, failModel4)
+                        isCheck(EMAIL_PATTERN, model4)
                 ),
                 () -> assertFalse(
-                        isCheck(EMAIL_PATTERN, failModel5)
+                        isCheck(EMAIL_PATTERN, model5)
                 ),
                 () -> assertFalse(
-                        isCheck(EMAIL_PATTERN, failModel6)
+                        isCheck(EMAIL_PATTERN, model6)
                 )
         );
 
+    }
+
+    @Test
+    void userIdPatternSuccessTest() {
+
+        final Pattern userIdPattern = USER_ID_PATTERN_FUNCTION.apply(new int[]{2, 8});
+
+        final String model1 = "sui21";
+        final String model2 = "325123";
+        final String model3 = "test";
+
+        assertAll(
+                () -> assertTrue(
+                        isCheck(userIdPattern, model1)
+                ),
+                () -> assertTrue(
+                        isCheck(userIdPattern, model2)
+                ),
+                () -> assertTrue(
+                        isCheck(userIdPattern, model3)
+                )
+        );
+    }
+
+    @Test
+    void userIdPatternFailTest() {
+
+        final Pattern userIdPattern = USER_ID_PATTERN_FUNCTION.apply(new int[]{2, 8});
+
+        final String model1 = "테스트";
+        final String model2 = "jfkdsng3234";
+        final String model3 = "2394324823";
+        final String model4 = "testtesttest";
+        final String model5 = "#@$#$";
+        final String model6 = "t";
+        final String model7 = "ㅎㄴㅇㄴ";
+
+        assertAll(
+                () -> assertFalse(
+                        isCheck(userIdPattern, model1)
+                ),
+                () -> assertFalse(
+                        isCheck(userIdPattern, model2)
+                ),
+                () -> assertFalse(
+                        isCheck(userIdPattern, model3)
+                ),
+                () -> assertFalse(
+                        isCheck(userIdPattern, model4)
+                ),
+                () -> assertFalse(
+                        isCheck(userIdPattern, model5)
+                ),
+                () -> assertFalse(
+                        isCheck(userIdPattern, model6)
+                ),
+                () -> assertFalse(
+                        isCheck(userIdPattern, model7)
+                )
+        );
     }
 }
--- src/test/java/io/keede7/pattern/model/SignupPatternTestModel.java
+++ src/test/java/io/keede7/pattern/model/SignupPatternTestModel.java
@@ -2,6 +2,7 @@ package io.keede7.pattern.model;
 
 import io.keede7.util.config.TestBaseRegexUtils;
 
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 /**
@@ -11,11 +12,24 @@ import java.util.regex.Pattern;
 public final class SignupPatternTestModel extends TestBaseRegexUtils {
 
     public static final Pattern EMAIL_PATTERN = Pattern.compile(combine(SignupPatternExpression.EMAIL));
+    public static final Function<int[], Pattern> USER_ID_PATTERN_FUNCTION =
+            range -> Pattern.compile(combineWithLength(range));
+
+    private static final int MIN_LENGTH = 2;
+
+    private static String combineWithLength(int... range) {
+        if(range.length < MIN_LENGTH) {
+            throw new RuntimeException("not enough range");
+        }
+
+        int min = range[0];
+        int max = range[1];
+        return combine(SignupPatternExpression.USER_ID + String.format("{%d,%d}", min, max));
+    }
 
     private static final class SignupPatternExpression {
-
         public static final String EMAIL = "[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-z]";
-
+        public static final String USER_ID = "[a-zA-Z0-9]";
     }
 
 }
